64f9d9c66af0b99c536c5273d3337a9ff4954959
suresh400 <70746945+suresh400@users.noreply.github.com>
2025-04-25 12:46:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from './context/ThemeContext';
 import Layout from './layouts/Layout';
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
+import Education from './components/Education';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certificates from './components/Certificates';
@@ -14,6 +15,7 @@ function App() {
       <Layout>
         <Hero />
         <AboutMe />
+        <Education />
         <Skills />
         <Projects />
         <Certificates />
